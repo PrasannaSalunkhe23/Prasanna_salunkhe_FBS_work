787106415210cbad68d7e66aa65c19df3818dc0c
Prasanna Salunkhe <prasannasalunkhe542@gmail.com>
2026-08-31 13:07:20 +0530

    Delete Core python/test directory

--- Core python/test/test1/Q3.py
+++
@@ -1,5 +0,0 @@
-#Write a program to accept distance in km and convert it into meters and centimeters both
-Distance=int(input("Enter a Distance in KM"))
-Meter=Distance*1000
-centimeters=Meter*100
-print(f'Meter={Meter} and Centimeters={centimeters}')
--- Core python/test/test5/Q2.py
+++
@@ -1,6 +0,0 @@
-n = int(input("enter number of coins:"))
-coins=list(map(int,input("enter coins:").split()))
-for i in coins:
-    if coins.count(i)%2!=0:
-        print("Missing coin:",i)
-        break
